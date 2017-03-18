2d455f40b212f956257d69cc694944a514d20672
Omer Ofir <omer@skycure.com>
2017-03-18 16:45:18 +0200

    Time in zulu

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ get '/' do
   content_type :json
   {
     uid: Time.now,
-    updateDate: Time.now,
+    updateDate: Time.now.utc.iso8601,
     titleText: 'Iba News',
     mainText: 'Maintextwat'
     # streamUrl: 'https://iba-news.herokuapp.com/last_news'
