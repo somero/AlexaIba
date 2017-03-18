fee035f28a42896941f5bf27d44d2f7f920142a4
Omer Ofir <omer@skycure.com>
2017-03-18 17:01:31 +0200

    Add iba

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'json'
 require 'securerandom'
 require 'open-uri'
+require 'nokogiri'
 
 set :protection, except: [:json_csrf]
 get '/' do
@@ -17,5 +18,52 @@ end
 
 get '/last_news' do
   content_type 'audio/mpeg'
-  open('http://www.orangefreesounds.com/wp-content/uploads/2017/01/Angry-dog-growling.mp3').read
+  open('/tmp/news.ts').read
+end
+
+def get_news
+  base = "www.iba.org.il"
+  url = URI.parse("http://#{base}/index.aspx?cat=1")
+  page = Nokogiri::HTML(open(url))
+  lastnews = page.css('.mahadorut li:first-child a').attribute('href').value
+
+  url = URI.parse("http://#{base}/#{lastnews}")
+  page = Nokogiri::HTML(open(url))
+  vimmid = page.css('#playerUrl').text
+  meta = metadata_url(vimmid)
+
+  metadata = Nokogiri::XML(open(meta))
+  playlist_url = metadata.at_css('SmilURL').text
+
+  pl =open(playlist_url) { |io| data = io.read }
+  chunk_list = pl.split("\n").select {|x| x.include? ('chunklist')}.first
+  chunks_url = "#{playlist_url.gsub(/\/playlist.*/,'')}/#{chunk_list}"
+
+  medias_file = open(chunks_url) { |io| data = io.read }
+  medias = medias_file.split("\n").select {|x| x.include? ('media')}
+  open("/tmp/join.txt", 'w') do |join|
+    medias.each_with_index { |m, i|
+      media_url = "#{playlist_url.gsub(/\/playlist.*/,'')}/#{m}"
+      open("/tmp/audio_part#{i}.ts", 'wb') do |file|
+        file << open(media_url).read
+      end
+      join.puts "file '/tmp/audio_part#{i}.ts'"
+    }
+  end
+
+  # Join files
+  `ffmpeg -f concat -safe 0 -i /tmp/join.txt -c copy /tmp/news.ts`
+  `rm /tmp/join.txt /tmp/audio*.ts`
+end
+
+
+def metadata_url(vimmiID)
+  deliveryType = 'hls'
+  siteName = 'iba'
+  accountName = 'iba'
+  searchString = '?smil_profile=default'
+  metaUrl = "http://" + accountName + "-metadata-rr-d.vidnt.com/"
+  metaUrl += "vod/vod/"
+  metaUrl += (vimmiID + "/" + deliveryType + "/metadata.xml")
+  metaUrl + searchString
 end
