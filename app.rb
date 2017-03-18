ff26bc8c819e3a96a84bb9b812d0e5f6dcc9f133
Omer Ofir <omer@skycure.com>
2017-03-18 16:48:59 +0200

    Audio back

--- app.rb
+++ app.rb
@@ -1,14 +1,19 @@
 require 'sinatra'
 require 'json'
+require 'securerandom'
 
 set :protection, except: [:json_csrf]
 get '/' do
   content_type :json
   {
-    uid: Time.now,
+    uid: SecureRandom.uuid,
     updateDate: Time.now.utc.iso8601,
     titleText: 'Iba News',
-    mainText: 'Maintextwat'
-    # streamUrl: 'https://iba-news.herokuapp.com/last_news'
+    mainText: '',
+    streamUrl: 'https://iba-news.herokuapp.com/last_news'
   }.to_json
+end
+
+get '/last_news' do
+  'bla'
 end
